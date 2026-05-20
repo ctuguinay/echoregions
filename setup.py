5caefe910c6877537aac088cfae7d8846a4067b6
Dominic Bashford <dominic.bashford@noaa.gov>
2026-05-20 16:08:56 -0600

    Delete old configuration files and update project url

--- setup.py
+++
@@ -1,3 +0,0 @@
-# This file is no longer needed and is kept for backward compatibility only.
-# All configuration has been migrated to pyproject.toml
-# This can be safely deleted in a future release.
